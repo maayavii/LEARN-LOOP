dbebbf51b730f6927d3b3a0b80db041b44ac0fca
Amal Maoj <amalmanoj2025@mca.sjcetpalai.ac.in>
2025-02-17 16:22:52 +0530

    Switched database from SQLite to PostgreSQL

--- myproject/settings.py
+++ myproject/settings.py
@@ -108,11 +108,14 @@ ASGI_APPLICATION = "myproject.routing.application"
 # Database settings
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
