79a448be2bfd2d4ff14aa3d74c291cb65e6b60e8
Amal Maoj <amalmanoj2025@mca.sjcetpalai.ac.in>
2025-02-17 10:23:21 +0530

    committed

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 DEBUG = os.getenv("DEBUG") == "True"
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
